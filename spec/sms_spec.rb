cff9a6f497d6185591af4fdac589e762492b97ac
Justin <justin.ison@gmail.com>
2013-12-17 13:54:20 -0500

    more updates

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -16,10 +16,10 @@ describe 'Testing my SMS application' do
       
     #get spreadsheet data
     gdoc = GoogleDocs.new
-    col1 = gdoc.getData(1,5) #(col1,row5) Set to max tc #. This really should be a variable...
-    col2 = gdoc.getData(2,5)
-    col3 = gdoc.getData(3,5)
-    #startc = gdoc.getData(4,0) #use this if you want to start at different test number.
+    col1 = gdoc.getData(1)
+    col2 = gdoc.getData(2)
+    col3 = gdoc.getData(3)
+    #startc = gdoc.getData(4) #use this if you want to start at different test number.
     
     col1.zip(col2, col3).each do |send, receive, test_num|
     #next if test_num.to_i < startc.to_i 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,17 +4,19 @@ require 'google_text'
 
 class GoogleDocs
     
-  def getData(column_num, count)
+  def getData(column_num)
     session = GoogleDrive.login($guser, $gpass)
     @ws = session.spreadsheet_by_key($gkey).worksheets[$work_sheet]
-    
+  
     row = 1
     col = column_num
     data = []
-     until row == count + 1 do
+    col_data = @ws[row, col].to_s
+     until col_data.empty? == true do
        col_data = @ws[row, col].to_s
        row = row + 1
        data << col_data.to_s
+       data.delete("")
      end
     data
   end
