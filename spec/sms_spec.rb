f62ae3e5a703c4c81b2a1bb19a08cb21c39f04da
Justin <justin.ison@gmail.com>
2014-11-11 16:08:45 +0100

    Update sms_spec.rb

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -6,10 +6,10 @@ describe 'Testing my SMS application' do
     
     if !GoogleText::Message.unread.empty?
       print "Marking old texts as read\n"
-        until GoogleText::Message.unread.count == 0 do
-          messages = GoogleText::Message.unread
-          messages.first.mark_as_read
-        end
+      until GoogleText::Message.unread.count == 0 do
+        messages = GoogleText::Message.unread
+        messages.first.mark_as_read
+      end
     end
       
     #get spreadsheet data
@@ -22,31 +22,29 @@ describe 'Testing my SMS application' do
     col1.zip(col2, col3).each do |send, receive, test_num|
     #next if test_num.to_i < startc.to_i 
     
-      puts ""
-      print "Testing case# #{test_num}\n"
-      print "Sending: #{send}\n"
+    puts ""
+    print "Testing case# #{test_num}\n"
+    print "Sending: #{send}\n"
     
-      message = GoogleText::Message.new(:text => send, :to => $text_to)
-      message.send
-      message.sent?.should == true
-    
-      messages = GoogleText::Message.unread
+    message = GoogleText::Message.new(:text => send, :to => $text_to)
+    message.send
+    message.sent?.should == true
+    messages = GoogleText::Message.unread
  
-      refresh = 1
-      until messages != [] do
-          puts "Waiting for sms reply, refreshing... #{refresh}"
-          messages = GoogleText::Message.unread
-          refresh = refresh + 1
-        if (refresh == $timeout) then
-          raise "NO SMS RECEIVED FOR TEST# #{test_num}\n"  
-          break       
-        end
+    refresh = 1
+    until messages != [] do
+      puts "Waiting for sms reply, refreshing... #{refresh}"
+      messages = GoogleText::Message.unread
+      refresh = refresh + 1
+      if (refresh == $timeout) then
+        raise "NO SMS RECEIVED FOR TEST# #{test_num}\n"  
+        break       
       end
+    end
     
-      print "Checking for response: #{receive}\n"
-      messages.first.text.should include(receive)
-      print "Good! Test Case# #{test_num} passed...\n"
-      messages.first.mark_as_read
-    end 
+    print "Checking for response: #{receive}\n"
+    messages.first.text.should include(receive)
+    print "Good! Test Case# #{test_num} passed...\n"
+    messages.first.mark_as_read
   end
 end
